0ff9ee8ef15a6a7075528480377f4eda4fbb75ec
4cozm <17328rm@gmail.com>
2024-05-29 00:52:05 +0900

    add:아이템 수정 로직 구현

--- src/controllers/itemControllers.js
+++ src/controllers/itemControllers.js
@@ -37,10 +37,58 @@ export const createItems = async (req, res) => {
         itemPrice: itemPrice,
       },
     });
-    return res.status(201).json({data:result});
+    return res.status(201).json({ data: result });
   } catch (error) {
     res
       .status(500)
       .json({ errorMessage: error.message + '////createItems에서 오류 발생' });
+  }
+};
+
+export const reNewItem = async (req, res) => {
+  try {
+    const { originalItemCode, itemCode, itemName, itemStat } = req.body;
+    //1.오리지날 아이템 코드를 기반으로 아이템을 찾는다.
+    const targetItem = await prisma.items.findFirst({
+      where: {
+        itemCode: originalItemCode,
+      },
+    });
+    if (!targetItem) {
+      return res
+        .status(401)
+        .json({
+          errorMessage: '해당하는 아이템 코드로 아이템을 찾을 수 없습니다',
+        });
+    }
+    //2.수정하려는 아이템 이름이 이미 존재하는지 확인한다
+    const nameExsist = await prisma.items.findFirst({
+      where: {
+        itemName: itemName,
+      },
+    });
+    if (nameExsist) {
+      return res
+        .status(401)
+        .json({
+          errorMessage: '변경하려고 하는 이름의 아이템이 이미 존재합니다',
+        });
+    }
+    //3.수정한다
+    const { health, power } = itemStat;
+    const result = await prisma.items.update({
+      where: {
+        itemCode: originalItemCode,
+      },
+      data: {
+        itemName: itemName,
+        itemCode: itemCode,
+        health: health,
+        power: power,
+      },
+    });
+    return res.status(201).json({ data: result });
+  } catch (error) {
+    return res.status(500).json({ errorMessage: error.message });
   }
 };
--- src/routers/items.js
+++ src/routers/items.js
@@ -1,12 +1,12 @@
 import express, { Router } from 'express';
 import authmiddleware from '../middleware/auth.middleware.js';
-import {createItems} from "../controllers/itemControllers.js";
+import {createItems,reNewItem} from "../controllers/itemControllers.js";
 
 
 const itemRouter = express.Router();
 
 itemRouter.post("/",createItems);
-
+itemRouter.patch("/",reNewItem);
 
 
 export default itemRouter;
