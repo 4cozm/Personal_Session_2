13d8af29591249500a6cb958429cd9893fc6c33d
4cozm <17328rm@gmail.com>
2024-05-29 01:22:04 +0900

    add:아이템 상세조회 로직 구현,itemStat 관련 DB수정, 아이템 로직 변경

--- src/controllers/itemControllers.js
+++ src/controllers/itemControllers.js
@@ -27,13 +27,13 @@ export const createItems = async (req, res) => {
         .json({ message: '이미 존재하는 아이템 코드입니다' });
     }
     //2.등록
-    const { power, health } = itemStat;
+    const {health,power} = itemStat; //스텟에서 필드 추출
+    const extractStat = {health,power}; // 추출한 필드를 다시 객체 변환
     const result = await prisma.items.create({
       data: {
         itemCode: +itemCode,
         itemName: itemName,
-        power: power,
-        health: health,
+        itemStat: extractStat,
         itemPrice: itemPrice,
       },
     });
@@ -55,11 +55,9 @@ export const reNewItem = async (req, res) => {
       },
     });
     if (!targetItem) {
-      return res
-        .status(401)
-        .json({
-          errorMessage: '해당하는 아이템 코드로 아이템을 찾을 수 없습니다',
-        });
+      return res.status(401).json({
+        errorMessage: '해당하는 아이템 코드로 아이템을 찾을 수 없습니다',
+      });
     }
     //2.수정하려는 아이템 이름이 이미 존재하는지 확인한다
     const nameExsist = await prisma.items.findFirst({
@@ -68,14 +66,13 @@ export const reNewItem = async (req, res) => {
       },
     });
     if (nameExsist) {
-      return res
-        .status(401)
-        .json({
-          errorMessage: '변경하려고 하는 이름의 아이템이 이미 존재합니다',
-        });
+      return res.status(401).json({
+        errorMessage: '변경하려고 하는 이름의 아이템이 이미 존재합니다',
+      });
     }
     //3.수정한다
     const { health, power } = itemStat;
+    const extractStat = {health,power};
     const result = await prisma.items.update({
       where: {
         itemCode: originalItemCode,
@@ -83,8 +80,7 @@ export const reNewItem = async (req, res) => {
       data: {
         itemName: itemName,
         itemCode: itemCode,
-        health: health,
-        power: power,
+        itemStat: extractStat
       },
     });
     return res.status(201).json({ data: result });
@@ -93,16 +89,40 @@ export const reNewItem = async (req, res) => {
   }
 };
 
+export const searchItems = async (req, res) => {
+  //1.모든 아이템의 코드,이름,가격
+  const result = await prisma.items.findMany({
+    select: {
+      itemId: true,
+      itemCode: true,
+      itemName: true,
+      itemPrice: true,
+    },
+  });
+  return res.status(200).json({ data: result });
+};
 
-export const searchItem = async (req, res) => {
-    //1.모든 아이템의 코드,이름,가격
-    const result = await prisma.items.findMany({
-        select: {
-            itemId: true,
-            itemCode: true,
-            itemName: true,
-            itemPrice: true
-        }
+export const searchOneItem = async (req, res) => {
+  try {
+    const itemID = parseInt(req.params.id);
+    const result = await prisma.items.findFirst({
+      where: {
+        itemCode: +itemID,
+      },
+      select: {
+        itemCode: true,
+        itemName: true,
+        itemStat: true,
+        itemPrice: true,
+      },
     });
+    if (!result) {
+      return res
+        .status(401)
+        .json({ message: '해당 코드의 아이템이 존재하지 않습니다' });
+    }
     return res.status(200).json({ data: result });
-}
+  } catch (error) {
+    return res.status(500).json({ errorMessage: error.message });
+  }
+};
--- src/routers/items.js
+++ src/routers/items.js
@@ -1,12 +1,12 @@
 import express, { Router } from 'express';
 import authmiddleware from '../middleware/auth.middleware.js';
-import {createItems,reNewItem,searchItem} from "../controllers/itemControllers.js";
+import {createItems,reNewItem,searchItems,searchOneItem} from "../controllers/itemControllers.js";
 
 
 const itemRouter = express.Router();
 
 itemRouter.post("/",createItems);
 itemRouter.patch("/",reNewItem);
-itemRouter.get("/",searchItem);
-
+itemRouter.get("/",searchItems);
+itemRouter.get("/:id",searchOneItem);
 export default itemRouter;
