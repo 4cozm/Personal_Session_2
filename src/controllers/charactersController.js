820044a242624f92010032bfd560a76776d5b6f0
4cozm <17328rm@gmail.com>
2024-05-28 23:57:10 +0900

    add:캐릭터 삭제

--- src/controllers/charactersController.js
+++ src/controllers/charactersController.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 export const createCharacters = async (req, res) => {
   try {
-    const { accountName, characterName,gender } = req.body;
+    const { accountName, characterName, gender } = req.body;
     //1. body에 전달해준 accountName과 미들웨어를 거친 user.accountName을 비교
     const user = req.user; //미들웨어를 거쳐 나온 user 정보
     if (user.accountName != accountName) {
@@ -16,12 +16,12 @@ export const createCharacters = async (req, res) => {
     } else {
       const newCharacter = await prisma.characters.create({
         data: {
-          accountId:user.accountId,//계정 주인 & 관계설정
-          characterName: characterName,//캐릭터 이름
-          gender:gender.toUpperCase(),
+          accountId: user.accountId, //계정 주인 & 관계설정
+          characterName: characterName, //캐릭터 이름
+          gender: gender.toUpperCase(),
         },
       });
-    //3.캐릭터 등록후 그 정보를 기반으로 인벤토리 자동 생성
+      //3.캐릭터 등록후 그 정보를 기반으로 인벤토리 자동 생성
       await prisma.inventory.create({
         data: {
           CharacterId: newCharacter.characterId,
@@ -34,6 +34,40 @@ export const createCharacters = async (req, res) => {
       .json({ message: '캐릭터가 성공적으로 생성되었습니다' + characterName });
   } catch (error) {
     res.status(500).json({ errorMessage: error.message });
+  }
+};
+
+export const deleteCharacter = async (req, res) => {
+  try {
+    //1.body에 전달받은 캐릭터 이름으로 삭제 진행
+    const { characterName } = req.body;
+    const character = await prisma.characters.findFirst({
+      where: {
+        characterName: characterName,
+      },
+    });
+    if (!character) {
+      return res
+        .status(404)
+        .json({ errorMessage: '캐릭터 이름을 찾을수 없습니다' });
+    }
+
+    //2.미들웨어에서 받아온 accountId를 기준으로 캐릭터 필드의 accountId가 동일한지 확인
+    const user = req.user;
+    if (user.accountId != character.accountId) {
+      return res
+        .status(401)
+        .json({ errorMessage: '캐릭터가 본인의 소유가 아닙니다' });
+    }
+    //3.삭제 진행
+    await prisma.characters.delete({
+      where:{
+        accountId:+user.accountId
+      }
+    });
+    return res.status(200).json({ message: '캐릭터 삭제 완료' });
+  } catch (error) {
+    return res.status(500).json({ errorMessage: error.message +"////deleteCharacter에서 오류 발생"});
   }
 };
 
--- src/routers/characters.js
+++ src/routers/characters.js
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
 import authmiddleware from '../middleware/auth.middleware.js';
-import { createCharacters } from '../controllers/charactersController.js';
+import { createCharacters,deleteCharacter } from '../controllers/charactersController.js';
 
 const charactersRouter = express(Router);
 
 charactersRouter.post('/', authmiddleware, createCharacters);
+charactersRouter.delete('/',authmiddleware,deleteCharacter);
 
 export default charactersRouter;
+
